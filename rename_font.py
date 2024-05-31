01f1b9fae8d708db016d95727bcd1195d9e8231e
easyhard007 <easyhard@qq.com>
2024-05-31 14:44:13 +0800

    Add files via upload

--- rename_font.py
+++ rename_font.py
@@ -1,9 +1,26 @@
 from fontTools.ttLib import TTFont, TTLibError
 import re
 import os
+import argparse
+
+# 默认目标文件夹
+folder_path = './'
+
+# 创建解析器
+parser = argparse.ArgumentParser(description='命令行参数解析')
 
 # 装有ttf或者otf字体的目标文件夹
-folder_path = 'C:\\Users\\easyh\\Desktop\\ziyou'
+parser.add_argument('-d', '--directory', type=str, help='目标文件夹')
+
+# 解析参数
+args = parser.parse_args()
+
+if(args.directory!=None):
+    folder_path = str(args.directory)
+else:
+    folder_path = './'
+
+
 
 # 判断字符串中是否包含中文
 def contains_chinese(text):
@@ -12,13 +29,26 @@ def contains_chinese(text):
 # 获取指定.ttf字体文件的内部中文名，如果没有中文名将使用英文名
 def get_font_chinese_name(ttfFile):
     font = TTFont(ttfFile)
-    c_name = ""
-    for font_name in font['name'].names:
-        if font_name.nameID == 1:  # 字体全名
-            c_name = font_name.toUnicode()
-            if contains_chinese(c_name):
-                break
-    return c_name
+    name_dict = {}  #名称字典, 如果有多个中文名，按优先级序列选择（简体中文>繁体中文,windows>mac），如果没有中文名，则按优先级选择英文名（英语美国>英语英国,windows>mac）
+    c_name_priority = [2052,3076, 36 ,1028, 19]
+    e_name_priority = [1033,0,2057]
+    for i,font_name in enumerate(font['name'].names):
+        # print(i,font_name,font_name.langID,font_name.nameID)
+        if font_name.nameID == 4: #包含字体家族详细信息:
+            name_dict[font_name.langID] = font_name.toUnicode()
+
+    # 按优先级序列搜索中文名，如果存在中文名直接返回中文名
+    for lang_id in c_name_priority:
+        if lang_id in name_dict.keys():
+            return name_dict[lang_id].replace(" ","-")
+
+    # 按优先级序列搜索英文名
+    for lang_id in e_name_priority:
+        if lang_id in name_dict.keys():
+            return name_dict[lang_id].replace(" ","-")
+
+    print("错误：",ttfFile,"无法找到中文名或英文名。")
+    return -1
 
 #重命名文件
 def rename_file(old_name, new_name):
@@ -32,16 +62,16 @@ def rename_file(old_name, new_name):
 if __name__ == "__main__":
     for root, dirs, files in os.walk(folder_path):
         for filename in files:
-            if filename.lower().endswith(".ttf") or filename.lower().endswith(".otf"):
+            if filename.lower().endswith(".ttf") or filename.lower().endswith(".otf") or filename.lower().endswith(".ttc"):
                 ttfFile = os.path.join(root, filename)
                 _, fileExtension = os.path.splitext(filename)
                 try:
                     c_name = get_font_chinese_name(ttfFile)
+                    if c_name==-1: continue
                     newFileName = os.path.join(root,c_name+fileExtension)
                     print(newFileName)
                     rename_file(ttfFile,newFileName)
                 except Exception as e:
                     print(e)
                     continue
-
 
